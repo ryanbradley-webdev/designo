58312561d61fbf68330e65ae65d3d30c0d9e83d2
Ryan Bradley <ryanbradley315@gmail.com>
2023-08-26 11:08:34 -0500

    Add homepage attributes subsection and mobile styling.

--- app/page.tsx
+++ app/page.tsx
@@ -68,6 +68,81 @@ export default function Home() {
         </ProjectLink>
 
       </section>
+
+      <section
+        className={styles.attributes}
+      >
+
+        <div>
+
+          <Image
+            src='/illustration-passionate.svg'
+            alt=''
+            height={202}
+            width={202}
+          />
+
+          <div>
+
+            <h3>
+              PASSIONATE
+            </h3>
+
+            <p>
+              Each project starts with an in-depth brand research to ensure we only create products that serve a purpose. We merge art, design, and technology into exciting new solutions.
+            </p>
+
+          </div>
+
+        </div>
+
+        <div>
+
+          <Image
+            src='/illustration-resourceful.svg'
+            alt=''
+            height={202}
+            width={202}
+          />
+
+          <div>
+
+            <h3>
+              RESOURCEFUL
+            </h3>
+
+            <p>
+              Everything that we do has a strategic purpose. We use an agile approach in all of our projects and value customer collaboration. It guarantees superior results that fulfill our clients&apos; needs.
+            </p>
+
+          </div>
+
+        </div>
+
+        <div>
+
+          <Image
+            src='/illustration-friendly.svg'
+            alt=''
+            height={202}
+            width={202}
+          />
+
+          <div>
+
+            <h3>
+              FRIENDLY
+            </h3>
+
+            <p>
+              We are a group of enthusiastic folks who know how to put people first. Our success depends on our customers, and we strive to give them the best experience a company can provide.
+            </p>
+
+          </div>
+
+        </div>
+
+      </section>
       
     </main>
   )
