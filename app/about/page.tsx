4b312e37969e463bfd611fa1e303c93009165879
Ryan Bradley <ryanbradley315@gmail.com>
2023-08-26 15:09:15 -0500

    Convert location grid to standalone component for use on contact page.

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,9 +1,7 @@
 import AboutCard from '@/components/aboutCard/AboutCard'
 import styles from './page.module.css'
 import ContactCard from '@/components/contactCard/ContactCard'
-import Button from '@/components/button/Button'
-import Link from 'next/link'
-import Image from 'next/image'
+import LocationGrid from '@/components/locationGrid/LocationGrid'
 
 export default function About() {
     return (
@@ -49,98 +47,7 @@ export default function About() {
 
             </AboutCard>
 
-            <section
-                className={styles.location_grid}
-            >
-
-                <div>
-
-                    <Image
-                        src='/illustration-canada.svg'
-                        alt=''
-                        height={202}
-                        width={202}
-                    />
-                    
-                    <div>
-
-                        <h3>
-                            CANADA
-                        </h3>
-
-                        <Button
-                            color='peach'
-                        >
-                            <Link
-                                href='/locations'
-                            >
-                                SEE LOCATION
-                            </Link>
-                        </Button>
-
-                    </div>
-
-                </div>
-
-                <div>
-
-                    <Image
-                        src='/illustration-australia.svg'
-                        alt=''
-                        height={202}
-                        width={202}
-                    />
-
-                    <div>
-
-                        <h3>
-                            AUSTRALIA
-                        </h3>
-
-                        <Button
-                            color='peach'
-                        >
-                            <Link
-                                href='/locations'
-                            >
-                                SEE LOCATION
-                            </Link>
-                        </Button>
-
-                    </div>
-
-                </div>
-
-                <div>
-
-                    <Image
-                        src='/illustration-united-kingdom.svg'
-                        alt=''
-                        height={202}
-                        width={202}
-                    />
-
-                    <div>
-
-                        <h3>
-                            UNITED KINGDOM
-                        </h3>
-
-                        <Button
-                            color='peach'
-                        >
-                            <Link
-                                href='/locations'
-                            >
-                                SEE LOCATION
-                            </Link>
-                        </Button>
-
-                    </div>
-
-                </div>
-
-            </section>
+            <LocationGrid />
 
             <AboutCard>
 
---
+++ components/locationGrid/LocationGrid.tsx
@@ -0,0 +1,101 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import Button from '../button/Button'
+import styles from './locationGrid.module.css'
+
+export default function LocationGrid() {
+    return (
+        <section
+            className={styles.location_grid}
+        >
+
+            <div>
+
+                <Image
+                    src='/illustration-canada.svg'
+                    alt=''
+                    height={202}
+                    width={202}
+                />
+                
+                <div>
+
+                    <h3>
+                        CANADA
+                    </h3>
+
+                    <Button
+                        color='peach'
+                    >
+                        <Link
+                            href='/locations'
+                        >
+                            SEE LOCATION
+                        </Link>
+                    </Button>
+
+                </div>
+
+            </div>
+
+            <div>
+
+                <Image
+                    src='/illustration-australia.svg'
+                    alt=''
+                    height={202}
+                    width={202}
+                />
+
+                <div>
+
+                    <h3>
+                        AUSTRALIA
+                    </h3>
+
+                    <Button
+                        color='peach'
+                    >
+                        <Link
+                            href='/locations'
+                        >
+                            SEE LOCATION
+                        </Link>
+                    </Button>
+
+                </div>
+
+            </div>
+
+            <div>
+
+                <Image
+                    src='/illustration-united-kingdom.svg'
+                    alt=''
+                    height={202}
+                    width={202}
+                />
+
+                <div>
+
+                    <h3>
+                        UNITED KINGDOM
+                    </h3>
+
+                    <Button
+                        color='peach'
+                    >
+                        <Link
+                            href='/locations'
+                        >
+                            SEE LOCATION
+                        </Link>
+                    </Button>
+
+                </div>
+
+            </div>
+
+        </section>
+    )
+}
