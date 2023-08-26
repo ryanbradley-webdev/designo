f4a65cb1282dd8fb3bc942e989e932586a0d181b
Ryan Bradley <ryanbradley315@gmail.com>
2023-08-26 09:18:35 -0500

    Add header and footer components.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
-import './globals.css'
 import type { Metadata } from 'next'
 import { Jost } from 'next/font/google'
+import Header from '@/components/header/Header'
+import Footer from '@/components/footer/Footer'
+import './globals.css'
 
 const jost = Jost({ subsets: ['latin'] })
 
@@ -16,7 +18,15 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={jost.className}>{children}</body>
+      <body className={jost.className}>
+
+        <Header />
+        
+        {children}
+
+        <Footer />
+        
+      </body>
     </html>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,9 @@ import styles from './page.module.css'
 export default function Home() {
   return (
     <main className={styles.main}>
-      Hello There
+      
+      
+      
     </main>
   )
 }
