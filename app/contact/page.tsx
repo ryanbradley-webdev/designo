513b52b0e87182da24a9e6ec6c75ac7b5c844d92
Ryan Bradley <ryanbradley315@gmail.com>
2023-08-31 14:04:50 -0500

    Remove min-height property from main to prevent glitch on certain screen sizes for contact page.

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,12 +1,9 @@
 import LocationGrid from '@/components/locationGrid/LocationGrid'
 import ContactForm from '@/components/contactForm/ContactForm'
-import styles from './contact.module.css'
 
 export default function Contact() {
     return (
-        <main
-            className={styles.main}
-        >
+        <main>
 
             <ContactForm />
 
